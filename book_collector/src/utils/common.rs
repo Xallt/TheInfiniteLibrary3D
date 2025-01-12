34dc25027a84314007b4b632ca7599469f55e4d2
Dmitry Shabat <mitya.shabat@gmail.com>
2025-01-12 21:30:16 +0200

    Remove unused type

--- book_collector/src/utils/common.rs
+++ book_collector/src/utils/common.rs
@@ -1,5 +1,4 @@
 pub trait SafeT: Send + Sync {}
 impl<T: Send + Sync> SafeT for T {}
 
-pub type SafeIterator<T: SafeT> = Box<dyn Iterator<Item = T> + Send + Sync>;
-pub type SafeResult<T: SafeT> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
+pub type SafeResult<SafeT> = Result<SafeT, Box<dyn std::error::Error + Send + Sync>>;
